1bf1c0dfa339a020a430a7ae7c8d753810aa06c8
konstantinmedvedev <medvedevblockchain@yandex.ru>
2024-07-11 09:57:22 +0500

    Оптимизация БД

--- back-end/repository/vote_repository.go
+++ back-end/repository/vote_repository.go
@@ -10,46 +10,6 @@ import (
 // Глобальная переменная для базы данных
 var db *sql.DB
 
-// InitDB инициализирует базу данных
-func InitDB(dataSourceName string) error {
-	var err error
-	db, err = sql.Open("sqlite3", dataSourceName)
-	if err != nil {
-		return err
-	}
-
-	// Создаем таблицу для голосований, если она не существует
-	createVotesTable := `
-	CREATE TABLE IF NOT EXISTS votes (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		title TEXT,
-		subtitle TEXT,
-		description TEXT,
-		voter TEXT,
-		choice TEXT,
-		vote_power INTEGER,
-		wallet_address TEXT
-	);`
-	if _, err := db.Exec(createVotesTable); err != nil {
-		return err
-	}
-
-	// Создаем таблицу для голосов пользователей, если она не существует
-	createUserVotesTable := `
-	CREATE TABLE IF NOT EXISTS user_votes (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		vote_id INTEGER,
-		voter TEXT,
-		choice TEXT,
-		vote_power INTEGER
-	);`
-	if _, err := db.Exec(createUserVotesTable); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // Карта силы голосов для различных кошельков
 var voteMap = map[string]int{
 	// (данные карты)
