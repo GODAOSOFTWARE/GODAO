c366cfc4707da35aa80d994ac527d69122376177
konstantinmedvedev <medvedevblockchain@yandex.ru>
2024-07-12 14:28:32 +0500

    Реализованы миграции базы данных

--- migrations/0002_create_user_votes_table.up.sql
+++ migrations/0002_create_user_votes_table.up.sql
@@ -1,8 +1,8 @@
 -- migrations/0002_create_user_votes_table.up.sql
-CREATE TABLE user_votes (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            vote_id INTEGER,
-                            voter TEXT,
-                            choice TEXT,
-                            vote_power INTEGER
+CREATE TABLE IF NOT EXISTS user_votes (
+                                          id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                          vote_id INTEGER,
+                                          voter TEXT,
+                                          choice TEXT,
+                                          vote_power INTEGER
 );
--- migrations/0003_create_vote_strength_table.up.sql
+++ migrations/0003_create_vote_strength_table.up.sql
@@ -1,6 +1,6 @@
 -- migrations/0003_create_vote_strength_table.up.sql
-CREATE TABLE vote_strength (
-                               id INTEGER PRIMARY KEY AUTOINCREMENT,
-                               wallet_address TEXT UNIQUE,
-                               vote_power INTEGER
+CREATE TABLE IF NOT EXISTS vote_strength (
+                                             id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                             wallet_address TEXT UNIQUE,
+                                             vote_power INTEGER
 );
